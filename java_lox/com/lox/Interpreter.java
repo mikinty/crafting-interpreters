2db8f4e0231bb5c2e75bf176763ce61c45dfb444
Michael You <youmichaelc@gmail.com>
2022-10-17 00:46:07 -0400

    Support printing out expressions again
    We still print out on re-assignments on variables, but not on
    declaration. I think this is fine, as I think most languages do this
    (e.g. JavaScript does).

--- java_lox/com/lox/Interpreter.java
+++ java_lox/com/lox/Interpreter.java
@@ -101,7 +101,8 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
-        evaluate(stmt.expression);
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
         return null;
     }
 
